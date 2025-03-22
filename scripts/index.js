329fbc7e638a74b32eae3a6cac60c5d636d49201
Camila Perez <camilaperez@Camilas-MacBook-Air.local>
2025-03-21 22:54:09 -0400

    added code to .js

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,9 @@ const inputOcupation = document.querySelector("#ocupation")
 const profileName = document.querySelector(".profile__about-name")
 const profileOcupation = document.querySelector(".profile__about-ocupation")
 
+const popup2 = document.querySelector(".popup2");
+const openModal2 = document.querySelector(".profile__button-add")
+const closeModal2 = document.querySelector(".popup__close-button2")
 
 
 if (openModal && popup && closeModal) {
@@ -20,6 +23,18 @@ closeModal.addEventListener("click", () => {
     popup.classList.remove("popup__show")
 });
 }
+
+
+if (openModal2 && popup2 && closeModal2) {
+    openModal2.addEventListener("click", (event) => {
+        event.preventDefault();
+        popup2.classList.add("popup__show")
+    });
+    
+    closeModal2.addEventListener("click", () => {
+        popup2.classList.remove("popup__show")
+    });
+    }
 
 
 formElement.addEventListener("submit", (event) => {
@@ -38,3 +53,75 @@ inputName.value = '';
 inputOcupation.value = '';
 
 });
+
+const initialCards = [
+    {
+      name: "Valle de Yosemite",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg"
+    },
+    {
+      name: "Lago Louise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg"
+    },
+    {
+      name: "Montañas Calvas",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg"
+    },
+    {
+      name: "Latemar",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg"
+    },
+    {
+      name: "Parque Nacional de la Vanoise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg"
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
+    }
+  ];
+
+
+const cards = document.querySelector("#cards");
+const cardTemplate = document.querySelector("#card-template");
+const inputTitle = document.querySelector("#titulo");
+const inputLink = document.querySelector("#link");
+const addButton = document.querySelector(".profile__button-add");
+
+function createCard (card) {
+const newNode = cardTemplate.content.querySelector(".card").cloneNode(true);
+newNode.querySelector(".card__img-container").src=card.link
+newNode.querySelector(".card__text").textContent=card.name
+newNode.querySelector(".card__delete-icon").addEventListener("click", function(){
+    newNode.remove();
+})
+
+return newNode;
+}
+
+function renderCards() {
+    initialCards.forEach(function(card){
+const newNode = createCard(card);
+cards.append(newNode);
+    });
+}
+renderCards();
+
+addButton.addEventListener("click", function(){
+    const name = inputName.value.trim();
+    const link = inputLink.value.trim();
+
+if (name && link) {
+    const newCard = {name, link};
+    initialCards.push(newCard);
+    const newNode = createCard(name, link);
+    cardTemplate.append(newNode);
+
+    inputName.value = "";
+    inputLink.value = "";
+} else {
+    alert("Por favor, ingrese su nombre y enlace válido.");
+}
+});
+
+  
